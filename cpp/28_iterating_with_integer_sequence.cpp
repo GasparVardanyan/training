7c3e503caa8e3672a3b736a7cdffd661a9453d02
GasparVardanyan <gaspar_pm@proton.me>
2026-02-14 02:34:48 +0400

    update

--- cpp/28_iterating_with_integer_sequence.cpp
+++ cpp/28_iterating_with_integer_sequence.cpp
@@ -27,49 +27,71 @@ struct GetTupleTypeCount <std::tuple <TS ...>>
 	: GetTypeCount <TS ...> {};
 
 template <typename Tuple, typename = std::make_index_sequence <GetTupleTypeCount <Tuple>::value>>
-struct TuplePrintHelper;
+struct TupleHelper;
 
 template <typename Tuple, std::size_t ... indices>
-struct TuplePrintHelper <Tuple, std::index_sequence <indices ...>> {
-	static std::ostream & print (std::ostream & os, const Tuple & tuple) {
-		((os << std::get <indices> (tuple) << ", "), ...);
+struct TupleHelper <Tuple, std::index_sequence <indices ...>> {
+	using tuple_t = std::decay_t <Tuple>;
+
+	template <typename ApplyTuple>
+	requires std::constructible_from <tuple_t, ApplyTuple>
+	static std::ostream & print (std::ostream & os, ApplyTuple && tuple) {
+		((os << std::get <indices> (std::forward <ApplyTuple> (tuple)) << ", "), ...);
 		return os;
+	}
+
+	template <typename F, typename ApplyTuple>
+	requires
+		   std::constructible_from <tuple_t, ApplyTuple>
+		&& std::invocable <F, decltype (std::get <indices> (std::declval <ApplyTuple> ())) ...>
+	static inline decltype (auto) apply (F && f, ApplyTuple && tuple) {
+		return std::forward <F> (f) (std::get <indices> (std::forward <ApplyTuple> (tuple)) ...);
 	}
 };
 
 template <typename ... TS>
 std::ostream & operator << (std::ostream & os, const std::tuple <TS ...> & tpl) {
-	TuplePrintHelper <std::tuple <TS ...>>::print (os, tpl);
+	TupleHelper <std::tuple <TS ...>>::print (os, tpl);
 
 	return os;
 }
 
-int main () {
-	std::cout << GetTypeCount <>::value << std::endl;
-	std::cout << GetTypeCount <int>::value << std::endl;
-	std::cout << GetTypeCount <int, float>::value << std::endl;
-	std::tuple <int, char> foo (10, 'x');
-	auto bar = std::make_tuple ("test", 3.1, 14, 'y');
+namespace detail {
+template <typename T>
+struct VerifyTuple {
+	static_assert (false, "T is not a tuple");
+};
 
+template <typename ... TS>
+struct VerifyTuple <std::tuple <TS ...>> {};
+
+template <typename F, typename Tuple, std::size_t ... idx>
+decltype (auto) apply (F && f, Tuple && tuple, std::index_sequence <idx ...> &&) {
+	(void) VerifyTuple <std::decay_t <Tuple>> {};
+	return std::forward <F> (f) (std::get <idx> (std::forward <Tuple> (tuple)) ...);
+}
+}
+
+template <typename F, typename Tuple>
+decltype (auto) MyApply (F && f, Tuple && tuple) {
+	return detail::apply (std::forward <F> (f), std::forward <Tuple> (tuple), std::make_index_sequence <std::tuple_size_v <std::decay_t <Tuple>>> {});
+}
+
+void func (const char * s, double d, int i, char c) {
+	std::cout << "\tstr: " << s << std::endl;
+	std::cout << "\tdouble: " << d << std::endl;
+	std::cout << "\tint: " << i << std::endl;
+	std::cout << "\tchar: " << c << std::endl;
+}
+
+int main () {
+	auto bar = std::make_tuple ("test", 3.1, 14, 'y');
+	apply (func, bar); // ADL find std::apply
 	std::cout << "TC: " << GetTupleTypeCount <decltype (bar)>::value << std::endl;
 	std::cout << bar << std::endl;
+	TupleHelper <decltype (bar)>::apply (func, bar);
+	std::cout << "====================" << std::endl;
+	MyApply (func, bar);
 
-	std::get <2> (bar) = 122;
-
-	int myint; char mychar;
-	std::tie (myint, mychar) = foo;
-
-	std::cout << myint << " - " << mychar << std::endl;
-
-	std::tie (std::ignore, std::ignore, myint, mychar) = bar;
-
-	std::cout << myint << " - " << mychar << std::endl;
-
-	mychar = std::get <3> (bar);
-	std::get <0> (foo) = std::get <2> (bar);
-	std::get <1> (foo) = std::get <3> (bar);
-
-	std::cout << std::get <0> (foo) << " - " << std::get <1> (foo) << std::endl;
-
-	std::cout << foo << std::endl;
+	return 0;
 }
