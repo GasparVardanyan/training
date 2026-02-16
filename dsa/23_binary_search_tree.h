79392104d5ed694cd030d36f87954e747b5be05b
GasparVardanyan <gaspar_pm@proton.me>
2026-02-16 23:22:34 +0400

    update

--- dsa/23_binary_search_tree.h
+++ dsa/23_binary_search_tree.h
@@ -5,34 +5,55 @@
 # include <functional>
 # include <iterator>
 # include <ostream>
+# include <type_traits>
 # include <utility>
 
 # include "20_vector.h"
 # include "23_binary_tree_node.h"
 
 namespace detail {
-namespace binary_search_tree__ {
-	template <typename T, typename Comparator>
-	requires std::strict_weak_order <Comparator, T, T>
-	struct equal_to {
-		static constexpr Comparator comparator {};
+template <typename T, typename Comparator>
+requires std::strict_weak_order <Comparator, T, T>
+struct equal_to {
+	static constexpr Comparator comparator {};
 
-		bool operator () (const T & first, const T & second) const {
-			return
-				   false == comparator (first, second)
-				&& false == comparator (second, first)
-			;
-		}
-	};
+	bool operator () (const T & first, const T & second) const {
+		return
+			   false == comparator (first, second)
+			&& false == comparator (second, first)
+		;
+	}
+};
 
-	template <typename T, typename U>
-	requires std::convertible_to <T, U>
-	struct equal_to <T, std::less <U>> : std::equal_to <U> {};
+template <typename T, typename U>
+requires std::convertible_to <T, U>
+struct equal_to <T, std::less <U>> : std::equal_to <U> {};
 
-	template <typename T, typename U>
-	requires std::convertible_to <T, U>
-	struct equal_to <T, std::greater <U>> : std::equal_to <U> {};
-}
+template <typename T, typename U>
+requires std::convertible_to <T, U>
+struct equal_to <T, std::greater <U>> : std::equal_to <U> {};
+
+template <typename T, typename Comparator>
+requires std::strict_weak_order <Comparator, T, T>
+struct binary_search_tree__ {
+	using equal_to = equal_to <T, Comparator>;
+	using node = binary_tree_node <T, equal_to>;
+
+	template <typename U>
+	static constexpr bool is_node_link_v =
+		std::is_pointer_v <U> &&
+		std::is_pointer_v <std::remove_pointer_t <U>> &&
+		std::is_same_v <
+			std::remove_cv_t <std::remove_pointer_t <
+				std::remove_cv_t <std::remove_pointer_t <
+					std::remove_cv_t <U>
+				>>
+			>>,
+			node
+		>
+	;
+
+};
 }
 
 // TODO: learn commenting: https://www.doxygen.nl/manual/docblocks.html
@@ -44,7 +65,8 @@ template <typename T, typename Comparator = std::less <T>>
 requires std::strict_weak_order <Comparator, T, T>
 class binary_search_tree {
 public:
-	using node = binary_tree_node <T, detail::binary_search_tree__::equal_to <T, Comparator>>;
+	using detail = detail::binary_search_tree__ <T, Comparator>;
+	using node = detail::node;
 	static constexpr Comparator less_than {};
 
 public:
@@ -180,11 +202,7 @@ public:
 			}
 			else {
 				if (nullptr != (* link)->left) {
-					node * leftmost = (* link)->right;
-
-					while (nullptr != leftmost->left) {
-						leftmost = leftmost->left;
-					}
+					node * leftmost = * getLinkToLeftmost (& (* link)->right);
 					leftmost->left = (* link)->left;
 				}
 
@@ -219,29 +237,11 @@ public:
 	}
 
 	const node * findMin () const {
-		if (nullptr == m_root) {
-			return nullptr;
-		}
-		else {
-			const node * n = m_root;
-			while (nullptr != n->left) {
-				n = n->left;
-			}
-			return n;
-		}
+		return * getLinkToLeftmost (& m_root);
 	}
 
 	const node * findMax () const {
-		if (nullptr == m_root) {
-			return nullptr;
-		}
-		else {
-			const node * n = m_root;
-			while (nullptr != n->right) {
-				n = n->right;
-			}
-			return n;
-		}
+		return * getLinkToRightmost (& m_root);
 	}
 
 	void makeEmpty () {
@@ -257,8 +257,8 @@ public:
 	bool empty () const { return 0 == m_size; }
 
 protected:
-	node * const * getLink (const T & value) const {
-		node * const * link = & m_root;
+	const node * const * getLink (const T & value) const {
+		const node * const * link = & m_root;
 
 		while (nullptr != * link) {
 			bool lt = less_than (value, (* link)->data);
@@ -282,25 +282,23 @@ protected:
 		return const_cast <node **> (const_cast <const binary_search_tree *> (this)->getLink (value));
 	}
 
-	stack <node * const *> getLinkStack (const T & value) const {
-		return _getLinkStack <node * const *> (value);
+	stack <const node * const *> getLinkStack (const T & value) const {
+		return getLinkStack <const node * const *> (& m_root, value);
 	}
 
 	stack <node **> getLinkStack (const T & value) {
-		return _getLinkStack <node **> (value);
+		return getLinkStack <node **> (& m_root, value);
 	}
 
 protected:
 	node * m_root;
 	std::size_t m_size;
 
-private:
+protected:
 	template <typename U>
-	requires std::is_same_v <std::remove_cv_t <U>, node **>
-	stack <U> _getLinkStack (const T & value) {
+	requires detail::template is_node_link_v <U>
+	static stack <U> getLinkStack (U link, const T & value) {
 		stack <U> linkStack;
-
-		U link = & m_root;
 		linkStack.push (link);
 
 		while (nullptr != * link) {
@@ -321,6 +319,30 @@ private:
 		}
 
 		return linkStack;
+	}
+
+	template <typename U>
+	requires detail::template is_node_link_v <U>
+	static U getLinkToLeftmost (U link) {
+		if (nullptr != * link) {
+			while (nullptr != (* link)->left) {
+				link = & (* link)->left;
+			}
+		}
+
+		return link;
+	}
+
+	template <typename U>
+	requires detail::template is_node_link_v <U>
+	static U getLinkToRightmost (U link) {
+		if (nullptr != * link) {
+			while (nullptr != (* link)->right) {
+				link = & (* link)->right;
+			}
+		}
+
+		return link;
 	}
 };
 
