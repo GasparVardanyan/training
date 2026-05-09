23f006450ccfd6abf356f14407d646d6dd305fc0
GasparVardanyan <gaspar_pm@proton.me>
2026-05-09 04:59:52 +0400

    backup

--- cpp/53_shared_ptr.cpp
+++ cpp/53_shared_ptr.cpp
@@ -1,28 +1,134 @@
 # include <algorithm>
+# include <array>
 # include <iostream>
 # include <iterator>
 # include <memory>
 # include <numeric>
 # include <cstddef>
 # include <tuple>
+# include <type_traits>
+# include <utility>
 
-int main () {
+namespace pre_cpp17 {
+	template <typename T>
+	struct MakeSharedArrayImpl;
+
+	template <typename T, std::size_t S>
+	struct MakeSharedArrayImpl <T [S]> {
+		constexpr std::shared_ptr <T> operator() ()
+			noexcept (std::is_nothrow_constructible_v <
+				std::shared_ptr <T>,
+				std::shared_ptr <std::array <T, S>> &,
+				decltype (std::declval <std::array <T, S>> ().data ())
+			>)
+		{
+			auto r = std::make_shared <std::array <T, S>> ();
+			if (nullptr == r) {
+				return {};
+			}
+			else {
+				// NOTE: aliasing constructor
+				// shares ownership with r, but wraps r->data ()
+				return std::shared_ptr <T> (r, r->data ());
+			}
+		}
+	};
+
+	template <typename Arr>
+	std::shared_ptr <std::remove_extent_t <Arr>> MakeSharedArray () {
+		return MakeSharedArrayImpl <Arr> {} ();
+	}
+}
+
+
+
+const std::size_t arr_size = 10;
+
+
+
+void shared_arrays () {
+	// shared pointers to arrays
+	std::shared_ptr <int []> arr1 = std::make_shared <int []> (arr_size);
+	std::shared_ptr <int []> arr2 (new int [arr_size]);
+
+	// shared pointers of ints
+	std::shared_ptr <int> arr3 (new int [arr_size], std::default_delete <int []>());
+	std::shared_ptr <int> arr4 = pre_cpp17::MakeSharedArray <int [arr_size]> ();
+
+	std::cout << "arrays:" << std::endl;
+	std::apply ([] (auto & ... arrays) -> void {
+		([& arrays] {
+			std::iota (arrays.get (), arrays.get () + arr_size, 1);
+			std::cout << '\t';
+			std::copy (arrays.get (), arrays.get () + arr_size, std::ostream_iterator <int> (std::cout, ", "));
+			std::cout << std::endl;
+		} (), ... );
+	}, std::tie (arr1, arr2, arr3, arr4));
+
+	std::cout << "array subscription:" << std::endl;
+	std::apply ([] (auto & ... arrays) -> void {
+		([&arrays] {
+			std::cout << '\t';
+			for (std::size_t i = 0; i < arr_size; i++) {
+				std::cout << arrays [i] << ", ";
+			}
+			std::cout << std::endl;
+		} (), ...);
+	}, std::tie (arr1, arr2));
+}
+
+
+
+void aliasing () {
+	struct S {
+		int v;
+	};
+	std::shared_ptr <int> sha2;
+
+	{
+		std::shared_ptr <S> sha1 (new S {.v = 10});
+		std::cout << "sha1 use count: " << sha1.use_count () << std::endl;
+		std::cout << "sha2 use count: " << sha2.use_count () << std::endl;
+		sha2 = std::shared_ptr <int> (sha1, & sha1->v);
+		std::cout << "sha1 use count: " << sha1.use_count () << std::endl;
+		std::cout << "sha2 use count: " << sha2.use_count () << std::endl;
+	}
+	std::cout << "sha2 use count: " << sha2.use_count () << std::endl;
+
+	std::cout << * sha2 << std::endl;
+}
+
+
+
+void ownership_transfer () {
 	std::shared_ptr <int> i = std::make_shared <int> (10);
 	std::shared_ptr <int> j;
 	j = i;
 	* j = 20;
 	std::cout << * i << std::endl;
 
-	const std::size_t arr_size = 10;
+	std::shared_ptr <int> k = std::move (i);
 
-	std::shared_ptr <int []> arr1 = std::make_shared <int []> (arr_size);
-	std::shared_ptr <int> arr2 (new int [arr_size] (0), std::default_delete <int []>());
+	std::cout << "i: " << i << ", j: " << j << ", k: " << k << std::endl;
+}
 
-	std::apply ([] (auto & ... arr) -> void {
-		([& arr] {
-			std::iota (arr.get (), arr.get () + arr_size, 1);
-			std::copy (arr.get (), arr.get () + arr_size, std::ostream_iterator <int> (std::cout, ", "));
-			std::cout << std::endl;
-		} (), ... );
-	}, std::tie (arr1, arr2));
+void shared_from_this () {
+	struct S : std::enable_shared_from_this <S> {
+		int value = 10;
+	};
+
+	S * s = new S;
+
+	std::shared_ptr <S> sh1 (s);
+	std::cout << "sh1 use count: " << sh1.use_count () << std::endl;
+	// std::shared_ptr <S> sh2 (s); // causes double free
+	std::shared_ptr <S> sh3 = s->shared_from_this ();
+	std::cout << "sh1 use count: " << sh1.use_count () << std::endl;
+	std::cout << "sh3 use count: " << sh3.use_count () << std::endl;
+}
+
+int main () {
+	shared_arrays ();
+	aliasing ();
+	shared_from_this ();
 }
---
+++ cpp/54_weak_ptr.cpp
@@ -0,0 +1,84 @@
+# include <cstddef>
+
+# include <iostream>
+# include <memory>
+# include <vector>
+
+struct TreeNode : std::enable_shared_from_this <TreeNode> {
+	int value;
+
+	explicit TreeNode (int value = 0) : value (value) {
+		std::cout << "creating node " << value << std::endl;
+	}
+
+	~TreeNode () {
+		std::cout << "deleting node " << value << std::endl;
+	}
+
+	std::weak_ptr <TreeNode> parent;
+	std::vector <std::shared_ptr <TreeNode>> children;
+
+	void addChild (std::shared_ptr <TreeNode> & node) {
+		// node->parent = weak_from_this ();
+		node->parent = shared_from_this (); // throws when the instance isn't managed
+											// by any shared_ptr
+		children.push_back (node);
+	}
+};
+
+int main () {
+	std::shared_ptr <TreeNode> root (new TreeNode);
+
+	for (std::size_t i = 0; i < 10; i++) {
+		std::shared_ptr <TreeNode> node (new TreeNode (10 + i));
+		root->addChild (node);
+
+		if (node->parent.lock () != root || root->children.back () != node) {
+			std::cout << "smth is wrong" << std::endl;
+		}
+	}
+
+	root.reset ();
+
+	struct S : std::enable_shared_from_this <S> {
+		explicit S (int n = 0) : value (n) {
+			std::cout << "S (" << value << ")" << std::endl;
+		}
+		~S () {
+			std::cout << "~S (" << value << ")" << std::endl;
+		}
+
+		int value;
+		std::shared_ptr <S> other;
+	};
+
+	{
+		S n (1);
+		std::cout << n.weak_from_this ().expired () << std::endl;
+		// n.shared_from_this (); // throws
+		S * np = new S (2);
+		std::cout << np->weak_from_this ().expired () << std::endl;
+		// np->shared_from_this ();  // throws
+		delete np;
+	}
+
+	{
+		std::shared_ptr <S> s1 (new S (10));
+		std::shared_ptr <S> s2 (new S (20));
+		s1->other = s2; s2->other = s1; // memory leak: no ~S() gets called
+	}
+
+	{
+		std::shared_ptr <TreeNode> n1 (new TreeNode (100));
+		std::shared_ptr <TreeNode> n2 (new TreeNode (101));
+		n1->parent = n2; n2->parent = n1; // no memory leak: parent is weak_ptr
+	}
+
+	{
+		std::shared_ptr <TreeNode> n1 (new TreeNode (1000));
+		std::shared_ptr <TreeNode> n2 (new TreeNode (1001));
+
+		// memory leak
+		n1->children.push_back (n2); n2->children.push_back (n1);
+	}
+}
